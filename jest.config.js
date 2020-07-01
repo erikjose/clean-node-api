21418a7fa80ee1f7012d64c8b8c73d4215663916
Erik José Silva <serikjose@gmail.com>
2020-07-01 10:39:45 -0300

    feat: add function

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,11 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-  roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
-    ".*\\.ts$": "ts-jest",
-  },
-};
+    '.*\\.ts$': 'ts-jest'
+  }
+}
